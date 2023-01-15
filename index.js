ae558321b24a59b5706424b0c4cb38784118ad59
Mubbashir <mohdmubbashir71@gmail.com>
2023-01-15 21:44:22 +0530

    all files are commited

---
+++ index.js
@@ -0,0 +1,31 @@
+const btn=document.getElementById('btn')
+const name=document.getElementById('name')
+let usn=document.getElementById('usn')
+const sem=document.getElementById('sem')
+const branch=document.getElementById('branch')
+const option=document.getElementById('option')
+const msg=document.getElementById('msg')
+btn.addEventListener("click",function(){
+const myname=name.value
+const myusn=usn.value
+const mysem=sem.value
+const mybranch=branch.value
+const myoption=option.value
+console.log(myname)
+if(myname=="" || myusn=="" || mysem=="" || mybranch=="" || myoption==""){
+msg.style.display="block"
+setInterval(() => {
+    msg.style.display="none"
+ 
+}, 3000);
+}
+else{
+localStorage.setItem('name',myname)
+localStorage.setItem('usn',myusn)
+localStorage.setItem('sem',mysem)
+localStorage.setItem('branch',mybranch)
+localStorage.setItem('option',myoption)
+window.location.href="letter.html"
+document.getElementById('form').reset()
+}
+})
